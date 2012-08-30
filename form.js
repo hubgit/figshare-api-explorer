bdd7f81245833aed909a22be880e35b15a99242a
Alf Eaton <alf@hubmed.org>
2012-08-30 19:02:15 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,68 @@
+$("form").on("submit", function(event) {
+	event.preventDefault();
+
+	var form = $(this);
+	var formToSubmit = form.clone();
+
+	// replace the placeholders in the form action URI template, and remove those inputs from the form
+	formToSubmit.attr("action", formToSubmit.attr("action").replace(/\{(\w+)\}/, function getFormValue(match, field) {
+		var node = formToSubmit.find("[name=" + field + "]");
+		if (!node.length) return "";
+
+		var value = node.val();
+		node.remove();
+
+		//return encodeURIComponent(value);
+		return value;
+	}));
+
+	form.find("select").each(function() {
+		var select = $(this);
+		formToSubmit.find("select[name=" + select.attr("name") + "]").val(select.val());
+	});
+
+	formToSubmit.find("input,select").each(function(index, node) {
+		var node = $(node);
+
+		if (!node.val()) {
+			node.remove();
+		}
+	});
+
+	formToSubmit.submit();
+});
+
+$("input[data-example]").each(function() {
+	var input = $(this);
+
+	var link = $("<a/>", { "class": "example", href: "#", text: input.data("example") }).insertAfter(input);
+});
+
+$(".example").on("click", function(event) {
+	event.preventDefault();
+
+	var example = $(this);
+
+	example.prev("input,select").val(example.text());
+
+	example.closest("form").submit();
+});
+
+$("input[name=has_category]").each(function() {
+	var input = $(this);
+
+	$.getJSON("categories.json", function(data) {
+		var select = $("<select/>", { name: input.attr("name") });
+
+		if (!input.attr("required")) {
+			$("<option/>", { value: "", text: "All Categories" }).appendTo(select);
+		}
+
+		data.items.forEach(function(item) {
+			$("<option/>", { value: item.id, text: item.name }).appendTo(select);
+		});
+
+		input.replaceWith(select);
+	});
+});
+
